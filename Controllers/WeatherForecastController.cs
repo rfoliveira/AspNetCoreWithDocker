3fd8fa019f846183abf17fe0420b6988b2109ae1
Rafaer Fernandes de Oliveira <rafaelolivei@gmail.com>
2021-05-19 17:51:33 -0300

    Subindo fontes

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreWithDocker.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 10).Select(index => index)
+            .ToArray();
+        }
+    }
+}
